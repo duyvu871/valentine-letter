5534b4ea9d19a356fe5f92bf5dbffec023084d36
duyvu871 <78553549+duyvu871@users.noreply.github.com>
2023-02-15 22:57:04 +0700

    fix baseURL

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@
 //          ////////////////////////////////////
 //         ||||||||||||||||||||||||||||||||||||
 
-const giftUrl = "https://duyvu871.github.io/valentine-letter"//"http://192.168.9.24:5501/public/"; //'https://valentine-letter.web.app/';
+const giftUrl = "https://duyvu871.github.io/valentine-letter/"//"http://192.168.9.24:5501/public/"; //'https://valentine-letter.web.app/';
 
 var config = {
   apiKey: "AIzaSyAPN93h6dTiJvI1PgVCHtjYt1dllWBnVP4",
