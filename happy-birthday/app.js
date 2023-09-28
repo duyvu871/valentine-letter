4afe019d8a324d371fff34f611b6d0bfdc042492
duyvu871 <zoocp123@gmail.com>
2023-09-28 23:37:36 +0700

    first commit push

---
+++ happy-birthday/app.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyAPN93h6dTiJvI1PgVCHtjYt1dllWBnVP4",
+    authDomain: "callmezoo.firebaseapp.com",
+    databaseURL: "https://callmezoo-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "callmezoo",
+    storageBucket: "callmezoo.appspot.com",
+    messagingSenderId: "860229264546",
+    appId: "1:860229264546:web:21fc2d85954af6ff5123fb",
+    measurementId: "G-0PM4GKLVVV"
+}
+firebase.initializeApp(config);
+//Real time database
+
+var database = firebase.database();
